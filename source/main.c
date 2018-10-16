db2378f841f8f206ea5f068ccf4eadfdc578d448
ev1l0rd <deepnavy@waifu.club>
2018-10-16 20:16:42 +0200

    This was still local

--- source/main.c
+++ source/main.c
@@ -14,9 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <stdio.h>
+#include <stdlib.h>
 #include <switch.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "struct.h"
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -33,17 +35,44 @@ void init_sdl()
     IMG_Init(IMG_INIT_JPG | IMG_INIT_JPG);    
 }
 
-void open_vn(char* path)
+CurrentStatus process_line(char* current_string)
+{
+    char* text_compare = "text";
+    if (strstr(current_string, text_compare) == text_compare)
+        return {};
+
+}
+
+void draw_updates(CurrentStatus* current_status, CurrentStatus* previous_status)
+{
+    // Draw background
+
+    // Draw foreground
+
+    // Draw text
+}
+
+void start_vn(char* path)
 {
     // Check for save data (prompt if found, and if so load it)
 
     // Open main file
     FILE *current_file = fopen(strcat(path, "/script/main.scr"), "r");
 
+    // Line counting
+    int lines_read = 0;
+    CurrentStatus previous_status = {"", "", "", false};
+
     // Play lines up until input
-    fgets(current_string, 8192, current_file);
-    process_line(current_string);
-    // Await key inputs
+    while(fgets(current_string, 8192, current_file) != NULL) {
+        CurrentStatus current_status = process_line(current_string);
+        draw_updates(&current_status, &previous_status);
+        // Await key inputs
+        if (current_status.user_input == true)
+        {
+
+        }
+    }
 }
 
 int main(int argc, char **argv)
